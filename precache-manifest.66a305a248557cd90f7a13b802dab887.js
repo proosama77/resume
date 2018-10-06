677967a0b419b133b9392932f89711918b308d37
proosama77 <proosama77@gmail.com>
2018-10-06 12:29:36 -0700

    Updates

---
+++ precache-manifest.66a305a248557cd90f7a13b802dab887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "0415c3917944616c0466",
+    "url": "/resume/static/js/main.0415c391.chunk.js"
+  },
+  {
+    "revision": "9d23d8c380cda12cbb88",
+    "url": "/resume/static/js/1.9d23d8c3.chunk.js"
+  },
+  {
+    "revision": "0415c3917944616c0466",
+    "url": "/resume/static/css/main.edce5ad2.chunk.css"
+  },
+  {
+    "revision": "84ed7ffbfe7a29ad56b8ca5107776229",
+    "url": "/resume/index.html"
+  }
+];
